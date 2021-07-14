8eccf4b9e03c0cae06f912d6dd39a2d0f83de757
Sebastian Salazar <salazars804@gmail.com>
2021-07-14 00:56:18 -0500

    add helper for upload dinamic files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,36 +1,17 @@
-const path = require("path");
 const { response } = require("express");
-const { v4: uuidv4 } = require("uuid");
 
-const cargarArchivo = (req, res = response) => {
+const { subirArchivo } = require("../helpers");
+
+const cargarArchivo = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({ msg: "No hay archivos en la peticion" });
     return;
   }
 
-  const { archivo } = req.files;
-  const nombreCut = archivo.name.split(".");
-  const extension = nombreCut[nombreCut.length - 1];
+  // Para extensiones de imagenes
+  const nombre = await subirArchivo(req.files);
 
-  // Validar extension
-  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
-
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `La extension ${extension} no es permitida - validas(${extensionesValidas})`,
-    });
-  }
-
-  const nombreTemp = uuidv4() + "." + extension;
-  const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  res.json({ nombre });
 };
 
 module.exports = {
